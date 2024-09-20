4228b07e19d5e677b9012a394f04229cbd8a132a
Montanher <gabrielmontanherr@gmail.com>
2024-09-20 15:02:50 -0300

    Updates

--- web/cypress/e2e/register.cy.js
+++ web/cypress/e2e/register.cy.js
@@ -3,12 +3,27 @@
 describe('Register of Orphanages', () => {
     it('Deve cadastrar um novo orfanato', () => {
         // eslint-disable-next-line no-undef
-        cy.visit('http://localhost:3000/orphanages/create')
+    //    cy.visit('http://localhost:3000/orphanages/create')
+
+    cy.visitWithMockGeolocation('http://localhost:3000/orphanages/create')
 
         cy.get('legend')
             .should('be.visible')
             .should('text.text', 'Cadastro')
 
         
+    })
+  })
+
+  Cypress.Commands.add('visitWithMockGeolocation', (url, latitude = 54, longitude = 39) => {
+    const mockGeolocation = (win, latitude, longitude) => {
+        cy.stub(win.navigator.geolocation, 'getCurrentPosition', cb => {
+            return cb({ coords: { latitude, longitude }});
+        });
+    };
+    cy.visit(url, {
+        onBeforeLoad: win => {
+            mockGeolocation(win, latitude, longitude);
+        }
     })
   })
