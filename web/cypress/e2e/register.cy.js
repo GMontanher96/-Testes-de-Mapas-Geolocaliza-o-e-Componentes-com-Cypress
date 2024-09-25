55bb555f438f48c52dc7662ba88cf45be2080a4f
Montanher <gabrielmontanherr@gmail.com>
2024-09-24 23:50:44 -0300

    updates

--- web/cypress/e2e/register.cy.js
+++ web/cypress/e2e/register.cy.js
@@ -1,9 +1,12 @@
 /// <reference types="cypress" />
+import data from '../fixtures/orphanages.json'
 
 describe('Register of Orphanages', () => {
     it('Deve cadastrar um novo orfanato', () => {
         // eslint-disable-next-line no-undef
     //    cy.visit('http://localhost:3000/orphanages/create')
+
+    const orphanage = data.create
 
     cy.visitWithMockGeolocation('http://localhost:3000/orphanages/create')
 
@@ -11,11 +14,25 @@ describe('Register of Orphanages', () => {
             .should('be.visible')
             .should('text.text', 'Cadastro')
 
+       //     cy.get('input[name=name]')
+      //      .type('Orfanato criança feliz')
+
+      cy.contains('label', 'Nome')
+        .parent()
+        .find('input').type(orphanage.name)
+
+        cy.get('#description').type(orphanage.description)
+
+        // horário de funcionamento
+        cy.get('#opening_hours').type(orphanage.opening_hours)
+
+        cy.contains('button', orphanage.open_on_weekends).click()
+
         
     })
   })
 
-  Cypress.Commands.add('visitWithMockGeolocation', (url, latitude = 54, longitude = 39) => {
+  Cypress.Commands.add('visitWithMockGeolocation', (url, latitude = -22.2767712, longitude = -49.0728608) => {
     const mockGeolocation = (win, latitude, longitude) => {
         cy.stub(win.navigator.geolocation, 'getCurrentPosition', cb => {
             return cb({ coords: { latitude, longitude }});
